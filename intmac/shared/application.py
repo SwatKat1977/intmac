e322d25eec55425eed2fa2e7daec0fddbed234cf
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-05-18 19:47:26 +0100

    correct copyright notice

--- intmac/shared/application.py
+++ intmac/shared/application.py
@@ -1,5 +1,5 @@
 '''
-Copyright 2014-2021 Secure Shed Project Dev Team
+Copyright 2014-2021 Integrated Test Management Suite
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
