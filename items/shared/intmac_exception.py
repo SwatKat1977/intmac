5912b667f511286feac4d638402781a32d4d0e88
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-05-18 20:04:27 +0100

    update exception class with new name

--- items/shared/intmac_exception.py
+++ items/shared/intmac_exception.py
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 
-class ItemscException(Exception):
+class ItemsException(Exception):
     ''' Definition of Items Exception base class '''
