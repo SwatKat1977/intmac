35c0184847f6979ffec60823c102cd71bc5fa4f1
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-05-22 13:00:59 +0100

    work in progress for handshake - integrating logger

--- items/gateway-api/views/handshake_view.py
+++ items/gateway-api/views/handshake_view.py
@@ -13,7 +13,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-from quart import Blueprint, request
+from http import HTTPStatus
+import json
+import logging
+import mimetypes
+from quart import Blueprint, request, Response
+from logging_consts import LOGGING_DATETIME_FORMAT_STRING, \
+                           LOGGING_DEFAULT_LOG_LEVEL, \
+                           LOGGING_LOG_FORMAT_STRING
 
 def create_handshake_blueprint():
     view = View()
@@ -31,14 +38,31 @@ def create_handshake_blueprint():
     return blueprint
 
 class View:
+    ''' Handshake view container class. '''
 
     def __init__(self):
-        print('AuthView init()')
+
+        self._logger = logging.getLogger(__name__)
+        log_format= logging.Formatter(LOGGING_LOG_FORMAT_STRING,
+                                      LOGGING_DATETIME_FORMAT_STRING)
+        console_stream = logging.StreamHandler()
+        console_stream.setFormatter(log_format)
+        self._logger.setLevel(LOGGING_DEFAULT_LOG_LEVEL)
+        self._logger.addHandler(console_stream)
+
+        mimetypes.init()
 
     def authenticate_user_handler(self, api_request):
-        print('Test: Authenticate')
-        return 'Test: Authenticate', 404
+        self._logger.debug("Generating fake auth response for now...")
+        resp = {
+            "status": "OK",
+            "token": "c7dd0a54-baea-11eb-8529-0242ac130003"
+
+        }
+        return Response(json.dumps(resp), status=HTTPStatus.OK,
+                        mimetype=mimetypes.types_map['.json'])
 
     def logout_user_handler(self, api_request):
-        print('Test: Logout')
-        return 'Test: Logout', 404
+        self._logger.debug("Logout is not implemented yet")
+        return Response('', status=HTTPStatus.OK,
+                        mimetype=mimetypes.types_map['.txt'])
