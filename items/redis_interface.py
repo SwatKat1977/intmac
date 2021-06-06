6c200bb594f601d88c7a8b3fd7e06b9b08b48798
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-06-06 13:16:31 +0100

    test code for redis interface

---
+++ items/redis_interface.py
@@ -0,0 +1,65 @@
+'''
+Copyright 2014-2021 Integrated Test Management Suite
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+import json
+import logging
+import redis
+from logging_consts import LOGGING_DATETIME_FORMAT_STRING, \
+                           LOGGING_DEFAULT_LOG_LEVEL, \
+                           LOGGING_LOG_FORMAT_STRING
+
+class RedisInterface:
+    __slots__ = ["_host", "_logger", "_port", "_redis"]
+
+    def __init__(self, host : str, port : int) -> None:
+        self._host = host
+        self._port = port
+        self._redis = None
+
+        self._logger = logging.getLogger(__name__)
+        log_format= logging.Formatter(LOGGING_LOG_FORMAT_STRING,
+                                      LOGGING_DATETIME_FORMAT_STRING)
+        console_stream = logging.StreamHandler()
+        console_stream.setFormatter(log_format)
+        self._logger.setLevel(LOGGING_DEFAULT_LOG_LEVEL)
+        self._logger.addHandler(console_stream)
+
+    def initialise(self) -> bool:
+        status = False
+
+        try:
+            self._redis = redis.Redis(host = self._host, port = self._port)
+            self._redis.get('invalid_key')
+            status = True
+
+        except redis.exceptions.ConnectionError as ex:
+            self._logger.critical('Unable to commect to redis, reason: %s', ex)
+
+        return status
+
+    def test1(self):
+        trial = {"str": "example", "int": 2001}
+        self._redis.set('jsondata', '{"str": "example", "int": 20}' )
+        self._redis.set('jsondata', json.dumps(trial) )
+
+    def test2(self):
+        t = self._redis.get('jsondata')
+        t = json.loads(t)
+        print(t)
+
+r = RedisInterface('localhost', '6379')
+r.initialise()
+r.test1()
+r.test2()
