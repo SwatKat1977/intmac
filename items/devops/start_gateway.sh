75c9391ad4197e30a474b3c74037800b15441d5b
SwatKat1977 <paulmorriss1977@gmail.com>
2023-04-30 17:00:03 +0100

    update start script

--- items/devops/start_gateway.sh
+++ items/devops/start_gateway.sh
@@ -1,6 +1,7 @@
 export PYTHONPATH=`pwd`/shared:`pwd`/gateway_api
 export QUART_APP=gateway_api
 export QUART_DEBUG=true
-export ITEMS_DATABASE_FILE=auth.db
-export ITEMS_CREATE_WHEN_MISSING=1
-quart run -p 3030
+export ITEMS_GATEWAY_SVC_CONFIG_FILE=configurations/gateway_svc.config
+export ITEMS_GATEWAY_SVC_CONFIG_FILE_REQUIRED=1
+export ITEMS_CREATE_WHEN_MISSING=0
+python3 -m quart run -p 3030
