98e0bec2eb7f6fb97ee48252970f9a113d978107
SwatKat1977 <paulmorriss1977@gmail.com>
2023-05-13 11:36:07 +0100

    work on compose

--- deployment/bootstrapDocker.sh
+++ deployment/bootstrapDocker.sh
@@ -28,17 +28,26 @@ verify_configuration_file()
     fi
 
     source $1
+
     invalid_config=0
+    internal_db_file=`pwd`/items_accounts_svc.db
 
     if [ -z "$GATEWAY_SVC_CONFIG" ] || [ ! -f $GATEWAY_SVC_CONFIG ] ; then
         warn "GATEWAY_SVC_CONFIG configuration item missing or file invalid"
         invalid_config=1
     fi
+    export GATEWAY_SVC_CONFIG=$GATEWAY_SVC_CONFIG
 
     if [ -z "$ACCOUNTS_SVC_CONFIG" ] || [ ! -f $ACCOUNTS_SVC_CONFIG ] ; then
         warn "ACCOUNTS_SVC_CONFIG configuration item missing or file invalid"
         invalid_config=1
     fi
+    export ACCOUNTS_SVC_CONFIG=$ACCOUNTS_SVC_CONFIG
+
+    if [ "$ACCOUNTS_SVC_INTERNAL_DB" ] ; then
+        internal_db_file=$ACCOUNTS_SVC_INTERNAL_DB
+    fi
+    export ACCOUNTS_SVC_INTERNAL_DB=$internal_db_file
 
     if [ -z "$PORTAL_SVC_CONFIG" ] || [ ! -f $PORTAL_SVC_CONFIG ] ; then
         warn "PORTAL_SVC_CONFIG configuration item missing or file invalid"
@@ -52,14 +61,44 @@ verify_configuration_file()
     msg "Configuration file is valid"
 }
 
+sweep_docker()
+{
+    msg "Performing Docker sweep"
+    msg "======================="
+
+    msg "Checking if Docket network 'items_docker_network' exists..."
+    if [ "$(docker network ls | grep items_docker_network)" ]; then
+        msg "Removing Docket network 'items_docker_network'."
+        docker network rm items_docker_network
+    fi
+
+    msg "Checking status of accounts service container"
+    if [ ! "$(docker ps | grep items_accounts_svc)" ]; then
+        if [ "$(docker ps -aq -f name=items_accounts_svc)" ]; then
+            # cleanup
+            msg "Removing Docker container 'items_accounts_svc'"
+            docker rm items_accounts_svc
+        fi
+
+    else
+        die "Svc already running. (TODO)"
+    fi
+}
+
 start_docker_images()
 {
     msg "Creating Docker network 'items_docker_network' ..."
     docker network create --driver bridge items_docker_network || exit 1
 
+    export ITEMS_ACCOUNTS_SVC_IMAGE="intmac/nightly:accounts-svc-$1"
+
+    docker compose up
+
     msg "Starting Web Portal Service..."
 
     msg "Starting Accounts Service..."
+
+    die "before"
 
     docker run -d \
         --name items_accounts_svc \
@@ -67,9 +106,6 @@ start_docker_images()
         -p 5050:5000 \
         --volume=$ACCOUNTS_SVC_CONFIG:/usr/local/items/accounts_svc.config \
         items_accounts_svc || exit 1
-
-    msg "keep trucking"
-
 }
 
 config_file='items_deployment.config'
@@ -108,4 +144,7 @@ if [ "$pull_images" ]; then
 fi
 
 verify_configuration_file $config_file
-start_docker_images
+
+sweep_docker
+
+start_docker_images $release
