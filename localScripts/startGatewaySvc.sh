48225ee15a103331b351db5969a6f93e3b542f27
SwatKat1977 <paulmorriss1977@gmail.com>
2023-05-13 12:11:29 +0100

    Fixing errors in docker file

--- localScripts/startGatewaySvc.sh
+++ localScripts/startGatewaySvc.sh
@@ -5,4 +5,4 @@ export QUART_DEBUG=true
 export ITEMS_GATEWAY_SVC_CONFIG_FILE=../configurations/gateway_svc.config
 export ITEMS_GATEWAY_SVC_CONFIG_FILE_REQUIRED=1
 export ITEMS_CREATE_WHEN_MISSING=0
-python3 -m quart run -p 4000
+python3 -m quart run -p 4050
