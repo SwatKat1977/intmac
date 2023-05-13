4d51f38132923d13758c96da5ab550ed1944d77c
SwatKat1977 <paulmorriss1977@gmail.com>
2023-05-13 19:13:54 +0100

    changes for configuration with docker

--- localScripts/startContentsManagementSvc.sh
+++ localScripts/startContentsManagementSvc.sh
@@ -4,4 +4,4 @@ export QUART_APP=cms_svc
 export QUART_DEBUG=true
 export ITEMS_CMS_SVC_CONFIG_FILE=../configurations/cms_svc.config
 export ITEMS_CMS_SVC_CONFIG_FILE_REQUIRED=1
-quart run -p 6000
+quart run -p 6050
