561b7bc5b6f92c9f17e69c8ba6b69ef294a8fa15
SwatKat1977 <paulmorriss1977@gmail.com>
2023-05-20 22:46:33 +0100

    Configuration changes

--- localScripts/startContentsManagementSvc.sh
+++ localScripts/startContentsManagementSvc.sh
@@ -4,4 +4,5 @@ export QUART_APP=cms_svc
 export QUART_DEBUG=true
 export ITEMS_CMS_SVC_CONFIG_FILE=../configurations/cms_svc.config
 export ITEMS_CMS_SVC_CONFIG_FILE_REQUIRED=1
+export BACKEND_INTERNAL_DB_FILENAME=../configurations/items_cms_svc.db
 quart run -p 6050
