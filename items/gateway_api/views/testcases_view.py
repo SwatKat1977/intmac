2ffb44540734e8c5cd601aa3bdd4a5a0852a9afd
SwatKat1977 <paulmorriss1977@gmail.com>
2023-05-27 04:04:49 +0100

    changes

--- items/gateway_api/views/testcases_view.py
+++ items/gateway_api/views/testcases_view.py
@@ -33,7 +33,7 @@ def create_blueprint(logger : logging.Logger):
 
     @blueprint.route('/internal/testcases/get_testcases_overview', methods=['GET'])
     async def get_testcases_overview_request():
-        return await view.get_testcases_request()
+        return await view.get_testcases_overview_request(request)
 
     return blueprint
 
@@ -57,17 +57,26 @@ class View(BaseView):
         'required' : ['project_id']
     }
 
+    GET_TESTCASES_OVERVIEW_REQUEST_SCHEMA = \
+    {
+        '$schema': 'http://json-schema.org/draft-07/schema#',
+
+        'type' : 'object',
+        'additionalProperties' : False,
+
+        'properties':
+        {
+            'project_id':
+            {
+                'type' : 'integer'
+            }
+        },
+        'required' : ['project_id']
+    }
+
     def __init__(self, logger : logging.Logger):
         self._logger : logging.Logger = logger.getChild(__name__)
         mimetypes.init()
-
-    '''
-        return await view.get_testsuites_request()
-
-    @blueprint.route('/internal/testcases/get_testcases_overview', methods=['GET'])
-    async def get_testcases_overview_request():
-        return await view.get_testcases_request()
-    '''
 
     async def get_testsuites_request(self, request_entry : request) -> Response:
         """
@@ -84,9 +93,9 @@ class View(BaseView):
             1) Is JSON format
             2) Validates against JSON schema
             '''
-            print("REQ", await request_entry.get_data())
             request_obj : ApiResponse = self._validate_json_body(
-                await request_entry.get_data(), self.GET_TESTSUITES_REQUEST_SCHEMA)
+                await request_entry.get_data(),
+                self.GET_TESTSUITES_REQUEST_SCHEMA)
 
             if request_obj.status_code != HTTPStatus.OK:
                 response_json = {
@@ -127,25 +136,88 @@ class View(BaseView):
 
             response_status = HTTPStatus.OK
 
-            print("RESP", api_response.body)
-
             response_json = {
                 "data":  api_response.body
             }
 
-            print("RESPONSE TO SEND:", response_json)
+        except requests.exceptions.ConnectionError as ex:
+            except_str = f"Internal error: {ex}"
+            self._logger.error(except_str)
 
-            # else:
-            #     token = uuid.uuid4().hex
-            #     self._sessions.add_auth_session(
-            #         request_obj.body.email_address, token, LogonType.BASIC)
+            response_json = {
+                "status":  0,
+                "error": str(ex)
+            }
+            response_status = HTTPStatus.INTERNAL_SERVER_ERROR
 
-            #     response_json = {
-            #         "status": 1,
-            #         "token": token
-            #     }
-            #     self._logger.info("User '%s' logged in",
-            #                       request_obj.body.email_address)
+        return Response(json.dumps(response_json), status = response_status,
+                                   mimetype = mimetypes.types_map['.json'])
+
+
+
+    async def get_testcases_overview_request(self, request_entry : request) -> Response:
+        """
+        Handler method to get overview of testcases for a given project.
+
+        returns:
+            Instance of Quart Response class.
+        """
+
+        try:
+            '''
+            STEP 1:
+            Validate the message body:
+            1) Is JSON format
+            2) Validates against JSON schema
+            '''
+            request_obj : ApiResponse = self._validate_json_body(
+                await request_entry.get_data(),
+                self.GET_TESTCASES_OVERVIEW_REQUEST_SCHEMA)
+
+            if request_obj.status_code != HTTPStatus.OK:
+                response_json = {
+                    'status': 'BAD',
+                    'error': request_obj.exception_msg
+                }
+                return Response(json.dumps(response_json),
+                                status=HTTPStatus.INTERNAL_SERVER_ERROR,
+                                mimetype=mimetypes.types_map['.json'])
+
+            '''
+            STEP 2:
+            Make API request from CMS service to get testsuite data.
+            '''
+            base_path : str = ThreadafeConfiguration().internal_apis_cms_svc
+
+            request : dict = {
+                "project_id": request_obj.body.project_id
+            }
+            url : str = (f'{base_path}'
+                         '/testcase/testcases')
+
+            api_response = await self._call_api_get(url, request)
+
+            print(api_response)
+
+            if api_response.status_code != HTTPStatus.OK:
+                print(api_response.body)
+                self._logger.critical("CMS svc request invalid - Reason: %s",
+                                      api_response.body['error'])
+                response_status = HTTPStatus.INTERNAL_SERVER_ERROR
+                response_json = {
+                    "status": 0,
+                    'error': api_response.body['error']
+                }
+
+                return Response(json.dumps(response_json),
+                                status=HTTPStatus.INTERNAL_SERVER_ERROR,
+                                mimetype=mimetypes.types_map['.json'])
+
+            response_status = HTTPStatus.OK
+
+            response_json = {
+                "data":  api_response.body
+            }
 
         except requests.exceptions.ConnectionError as ex:
             except_str = f"Internal error: {ex}"
