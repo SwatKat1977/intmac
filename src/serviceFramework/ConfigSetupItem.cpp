905c0a426664d629304f958b59a4e86fbe9d0f7e
SwatKat1977 <paulmorriss1977@gmail.com>
2023-12-03 11:26:36 +0000

    config setup items

---
+++ src/serviceFramework/ConfigSetupItem.cpp
@@ -0,0 +1,119 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of ITEMS
+(Integrated Test Management Suite )
+For the latest info, see https://github.com/SwatKat1977/intmac/
+
+Copyright 2014-2023 Integrated Test Management Suite Development Team
+
+    This program is free software : you can redistribute it and /or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https://www.gnu.org/licenses/>.
+-----------------------------------------------------------------------------
+*/
+#include "ConfigSetupItem.h"
+
+namespace items
+{
+    namespace serviceFramework
+    {
+    ConfigSetupItem::ConfigSetupItem (
+        std::string itemName,
+        ConfigItemDataType dataType) : m_itemName(itemName),
+                                       m_isRequired(false),
+                                       m_itemType(dataType),
+                                       m_isUnset(true),
+                                       m_defaultValueInt(0),
+                                       m_defaultValueStr("")
+    {
+    }
+
+    std::string ConfigSetupItem::GetItemName ()
+    {
+        return m_itemName;
+    }
+
+    ConfigItemDataType ConfigSetupItem::GetType()
+    {
+        return m_itemType;
+    }
+
+    bool ConfigSetupItem::GetIsUnset ()
+    {
+        return m_isUnset;
+    }
+
+    bool ConfigSetupItem::GetIsRequired ()
+    {
+        return m_isRequired;
+    }
+
+    int ConfigSetupItem::GetDefaultIntValue ()
+    {
+        return m_defaultValueInt;
+    }
+
+    std::string ConfigSetupItem::GetDefaultStringValue ()
+    {
+        return m_defaultValueStr;
+    }
+
+    IntList ConfigSetupItem::GetValidIntValues ()
+    {
+        return m_validValuesInt;
+    }
+
+    StringList ConfigSetupItem::GetValidStringValues ()
+    {
+        return m_validValuesStr;
+    }
+
+    ConfigSetupItem &ConfigSetupItem::SetIsRequired ()
+    {
+        m_isRequired = true;
+        return *this;
+    }
+
+    ConfigSetupItem &ConfigSetupItem::SetDefaultValue (int defaultValue)
+    {
+        m_defaultValueInt = defaultValue;
+        m_isUnset = false;
+        return *this;
+    }
+
+    ConfigSetupItem &ConfigSetupItem::SetDefaultValue(std::string defaultValue)
+    {
+        m_defaultValueStr = defaultValue;
+        m_isUnset = false;
+        return *this;
+    }
+
+    ConfigSetupItem &ConfigSetupItem::SetValidValues(IntList validValue)
+    {
+        m_validValuesInt = validValue;
+        return *this;
+    }
+
+    ConfigSetupItem &ConfigSetupItem::SetValidValues(StringList validValue)
+    {
+        m_validValuesStr = validValue;
+        return *this;
+    }
+
+    }   // namespace serviceFramework
+}   // namespace items
+
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
