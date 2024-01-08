fa96853b7727ae5e16c988c482989f7ad4b325c3
Gemma Morriss <paulmorriss1977@gmail.com>
2024-01-08 02:54:47 +0000

    dto

--- src/itemsAccountsSvc/DTOs/BasicAuthDTOs.h
+++ src/itemsAccountsSvc/DTOs/BasicAuthDTOs.h
@@ -1,129 +1,43 @@
-'''
-Copyright 2014-2023 Integrated Test Management Suite
+/*
+-----------------------------------------------------------------------------
+This source file is part of ITEMS
+(Integrated Test Management Suite)
+For the latest info, see https://github.com/SwatKat1977/intmac/
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Copyright 2014-2023 Integrated Test Management Suite Development Team
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    This program is free software : you can redistribute it and /or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-'''
-from http import HTTPStatus
-import json
-import logging
-import mimetypes
-from quart import Blueprint, request, Response
-from base_view import ApiResponse, BaseView
-from logging_consts import LOGGING_DATETIME_FORMAT_STRING, \
-                           LOGGING_DEFAULT_LOG_LEVEL, \
-                           LOGGING_LOG_FORMAT_STRING
-from logon_type import LogonType
-from sqlite_interface import SqliteInterface
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
 
-// Data Transfer Object
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https://www.gnu.org/licenses/>.
+-----------------------------------------------------------------------------
+*/
+#include "oatpp/network/tcp/server/ConnectionProvider.hpp"
 
-def create_basic_auth_blueprint(sql_interface : SqliteInterface,
-                                logger : logging.Logger):
-    view = View(sql_interface, logger)
-
-    blueprint = Blueprint('basic_auth_api', __name__)
-
-    @blueprint.route('/basic_auth/authenticate', methods=['POST'])
-    async def authenticate_request():
-        return await view.authenticate(request)
-
-    return blueprint
-
-class AuthenticateRequest:
-    ''' Definition of the basic_auth/authenticate request '''
-    #pylint: disable=too-few-public-methods
-
-    identifier = 'identifier'
-
-    schema = \
+namespace items
+{
+    namespace accountsSvc
     {
-        "$schema": "http://json-schema.org/draft-07/schema#",
 
-        "type" : "object",
-        "additionalProperties" : False,
-
-        "properties":
+        // Definition of the basic_auth authenticate request.
+        #include OATPP_CODEGEN_BEGIN(DTO)
+        class BasicAuthenticateRequest : public oatpp::DTO
         {
-            "email_address":
-            {
-                "type" : "string"
-            },
-            "password":
-            {
-                "type" : "string"
-            },
-        },
-        "required" : ["email_address", "password"]
-    }
+            DTO_INIT (BasicAuthenticateRequest, DTO)
 
-class View(BaseView):
-    __slots__ = ['_logger', '_sql_interface']
+            DTO_FIELD (String, email_address);
+            DTO_FIELD (String, password);
+        };
+        #include OATPP_CODEGEN_END(DTO)
 
-    def __init__(self, sql_interface : SqliteInterface,
-                 logger : logging.Logger) -> None:
-        self._sql_interface = sql_interface
-        self._logger = logger.getChild(__name__)
+    }   // namespace accountsSvc
+}   // namespace items
 
-    async def authenticate(self, api_request):
-
-        response : ApiResponse = self._validate_json_body(
-            await api_request.get_data(), AuthenticateRequest.schema)
-
-        if response.status_code != HTTPStatus.OK:
-            response_json = {
-                'status': 0,
-                'error': response.exception_msg
-            }
-            return Response(json.dumps(response_json),
-                            status=HTTPStatus.INTERNAL_SERVER_ERROR,
-                            mimetype=mimetypes.types_map['.json'])
-
-        try:
-            user_id, _ = self._sql_interface.valid_user_to_logon(
-                response.body.email_address, LogonType.BASIC)
-
-            if user_id:
-                status, err_str = self._sql_interface.basic_user_authenticate(
-                    user_id, response.body.password)
-
-                response_status = HTTPStatus.OK
-
-                if status:
-                    response_json = {
-                        'status': 1,
-                        'error': ''
-                    }
-
-                else:
-                    response_json = {
-                        'status': 0,
-                        'error': err_str
-                    }
-
-            else:
-                response_json = {
-                    'status': 0,
-                    'error': 'Invalid username/password'
-                }
-                response_status = HTTPStatus.OK
-
-        except RuntimeError as ex:
-            self._logger.critical(ex)
-            response_json = {
-                'status': 0,
-                'error': "Internal server error"
-            }
-            response_status = HTTPStatus.INTERNAL_SERVER_ERROR
-
-        return Response(json.dumps(response_json), status = response_status,
-                                   mimetype = mimetypes.types_map['.json'])
