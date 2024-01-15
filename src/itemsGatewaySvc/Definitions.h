9cadf75414509885739468f0192dd73d44a735fc
Gemma Morriss <paulmorriss1977@gmail.com>
2024-01-15 01:09:56 +0000

    framework skeleton

---
+++ src/itemsGatewaySvc/Definitions.h
@@ -0,0 +1,33 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of ITEMS
+(Integrated Test Management Suite )
+For the latest info, see https://github.com/SwatKat1977/intmac/
+
+Copyright 2014-2023 Integrated Test Management Suite Development Team
+
+    This program is free software : you can redistribute it and /or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https://www.gnu.org/licenses/>.
+-----------------------------------------------------------------------------
+*/
+#include <string>
+
+namespace items
+{
+    namespace gatewaySvc
+    {
+        const std::string SERVICE_PROVIDER_API_NAME = "api";
+        const int SERVICE_PROVIDER_API_PORT = 4000;
+
+    }   // namespace gatewaySvc
+}   // namespace items
