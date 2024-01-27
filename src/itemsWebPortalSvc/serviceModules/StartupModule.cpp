841996f71f792a31a4417d5ce2c9135604cb2d86
Gemma Morriss <paulmorriss1977@gmail.com>
2024-01-27 22:14:17 +0000

    startup module

---
+++ src/itemsWebPortalSvc/serviceModules/StartupModule.cpp
@@ -0,0 +1,76 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of ITEMS
+(Integrated Test Management Suite)
+For the latest info, see https://github.com/SwatKat1977/intmac/
+
+Copyright 2014-2023 Integrated Test Management Suite Development Team
+
+    This program is free software : you can redistribute it and /or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https://www.gnu.org/licenses/>.
+-----------------------------------------------------------------------------
+*/
+#include <filesystem>
+#include "oatpp/network/tcp/client/ConnectionProvider.hpp"
+#include "spdlog/spdlog.h"
+#include "ConfigurationLayout.h"
+#include "Definitions.h"
+#include "StartupModule.h"
+#include "Logger.h"
+#include "Version.h"
+
+namespace items { namespace webPortalSvc {
+
+    using namespace serviceFramework;
+
+    StartupModule::StartupModule (std::string name)
+        : ServiceModule (name)
+    {
+    }
+
+    bool StartupModule::Initialise ()
+    {
+        LOGGER->info ("ITEMS Gateway Microservice V{0}.{1}.{2}-{3}",
+            common::VERSION_MAJOR, common::VERSION_MINOR,
+            common::VERSION_PATCH, common::VERSION_TAG);
+        LOGGER->info (common::COPYRIGHT_TEXT);
+        LOGGER->info (common::LICENSE_TEXT);
+
+        LOGGER->info ("|=====================|");
+        LOGGER->info ("|=== Configuration ===|");
+        LOGGER->info ("|=====================|");
+
+        LOGGER->info ("[LOGGING]");
+        LOGGER->info ("-> Log Level      : {0}",
+            m_context->GetConfigManager ().GetStringEntry (
+                "logging", "log_level").c_str ());
+        LOGGER->info ("-> Log To Console : {0}",
+            m_context->GetConfigManager ().GetStringEntry (
+                "logging", "log_to_console").c_str ());
+        LOGGER->info ("-> Log Filename   : {0}",
+            m_context->GetConfigManager ().GetStringEntry (
+                "logging", "log_filename").c_str ());
+        LOGGER->info ("-> Max File Size  : {0:d}",
+            m_context->GetConfigManager ().GetIntEntry (
+                "logging", "max_file_size"));
+        LOGGER->info ("-> Max File Count : {0:d}",
+            m_context->GetConfigManager ().GetIntEntry (
+                "logging", "max_file_count"));
+        LOGGER->info ("-> Log Format     : {0}",
+            m_context->GetConfigManager ().GetStringEntry (
+                "logging", "log_format").c_str ());
+
+        return true;
+    }
+
+} }   // namespace items::webPortalSvc
